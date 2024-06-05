87e5a3c939c3f45e5485c6473c91b273e0d643c1
George Williams <george@localhost.localdomain>
2024-06-04 21:19:59 -0400

    adds excercise data

---
+++ stores/muscle-group.js
@@ -0,0 +1,28 @@
+const muscles = [
+    {
+        name: "shoulders",
+        number: 2
+    },
+    {
+        name: "arms",
+        number: 2
+    },
+    {
+        name: "chest",
+        number: 2
+    },
+    {
+        name: "back",
+        number: 2
+    },
+    {
+        name: "upper legs",
+        number: 2
+    },
+    {
+        name: "lower legs",
+        number: 2
+    },
+];
+
+module.exports = muscles;
