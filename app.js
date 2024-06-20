c21c338ee9f52b270c03c3179995354309ae917f
George Williams <george@localhost.localdomain>
2024-06-20 11:19:04 -0400

    reset excercise

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const planDay = require("./modules/day-plan");
-const { addWeights, findExcercise } = require("./modules/excercise");
 const createSplit = require("./modules/split");
 const repsAndWeight = require("./modules/weight-rep");
 
@@ -10,7 +9,6 @@ function generateWeek(maxLift, weights, days) {
 
     for(let x in weekDays) {
         weekDays[x] = planDay(weekDays[x]);
-        weekDays[x] = weekDays[x].map(findExcercise);
         
         if(weekDays[x].length > 0) {
             weekDays[x] = repsAndWeight(weekDays[x], maxLift)
--- modules/excercise.js
+++ modules/excercise.js
@@ -1,48 +1,5 @@
-const exercises = require("../stores/excercise-bank");
+function findExercises() {
 
-function addWeights(weights) {
-    if(typeof weights !== 'string') {
-        throw new Error('Must be a json string array');
-    }
-
-    let newWeights;
-    try {
-        newWeights = JSON.parse(weights.replace('\left[', '[').replace('\right]', ']'));
-
-        if(!Array.isArray(newWeights)) {
-            throw new Error('Must be a json string array');
-        }
-    } catch(e) {
-        throw new Error('Must be a json string array');
-    }
-
-    exercises.forEach((excercise, index) => {
-        excercise.weight = newWeights[index];
-    });
 }
 
-const getExcercise = (excercise) => {
-    const matchingExcercises = exercises.filter(excer => excer.muscleGroup.some(muscleOne => excercise.muscles.some(muscleTwo => muscleOne == muscleTwo)));
-    return matchingExcercises[Math.floor(Math.random() * matchingExcercises.length)];
-}
-
-function findExcercise(excercise, index, whole) {
-    let excerciseObj = getExcercise(excercise);
-
-    if(whole){
-        while(whole.some(excer => excer.name == excerciseObj.name)) {
-            excerciseObj = getExcercise(excercise);
-        }
-    }
-
-    return {
-        ...excerciseObj,
-        ...excercise,
-        number: exercises.findIndex(excer => excer.name == excerciseObj.name) + 3,
-    }
-}
-
-module.exports = {
-    findExcercise,
-    addWeights
-};
+module.exports = findExercises;
