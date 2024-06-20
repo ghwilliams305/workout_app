2dcf03230ad4e757ccf54dbba10ea6696bb52f30
George Williams <george@localhost.localdomain>
2024-06-20 19:50:39 -0400

    re-adds findExcercises function

--- modules/excercise.js
+++ modules/excercise.js
@@ -1,5 +1,49 @@
-function findExercises() {
+const exercises = require("../stores/excercise-bank");
 
+
+function findExercises(workout, weights) {
+    if(!Array.isArray(workout)) {
+        throw new Error(`${workout} is not an array`);
+    } else if(typeof weights !== 'string') {
+        throw new Error(`${weights} is not a string`);
+    }
+
+    try {
+        const weightArray = JSON.parse(weights);
+
+        weightArray.forEach((weight, index) => {
+            exercises[index].weight = weight;
+        });
+    } catch(e) {
+        throw new Error(`${weights} is a bad string`);
+    }
+
+    try {
+        const usedIndexes = [];
+
+        return workout.map(excercise => {
+            const filteredExcerises = exercises.filter(({muscleGroup}) => muscleGroup.some(muscleOne => excercise.muscles.some(muscleTwo => muscleOne == muscleTwo)));
+            let ranIndex, number;
+
+            do {
+                ranIndex = Math.floor(Math.random() * filteredExcerises.length);
+                
+                const name = filteredExcerises[ranIndex].name;
+                number = exercises.findIndex(excer => excer.name === name) + 3;
+            } while(usedIndexes.some(i => i == number));
+
+            const choosenExcer = filteredExcerises[ranIndex];
+            usedIndexes.push(number);
+
+            return {
+                ...excercise,
+                ...choosenExcer,
+                number: number,
+            }
+        });
+    } catch(e) {
+        throw new Error('Bad objects as elements');
+    }
 }
 
 module.exports = findExercises;
--- tests/excercise.test.js
+++ tests/excercise.test.js
@@ -1,3 +1,4 @@
+const findExercises = require("../modules/excercise");
 const exercises = require("../stores/excercise-bank");
 
 const sampleExcercise = [
@@ -14,9 +15,145 @@ const sampleExcercise = [
         repRange: 2,
     }
 ];
-const sampleWeights = '\left[115,145,135,130,115,70,30,35,250,140,65,235,125,180,135,55,75,75,75,90,295,285,210,170,150,75,115,95,75,95,235,115,40,25,60,30,190,135,120,120,150,25,80,90,35,85,55,240,195,80,30,35,65,140,155,145,65,65,120,70,85,65,65,100,50,65,25\right]';
-const sampleWeightsOne = '\left[115,145,135,130,115,70,30,35,250,140,65,235,125,180,135,55,75,75,75,90,295,285,210,170,150,75,115,95,75,95,235,110,40,25,60,30,190,135,120,120,150,25,85,90,35,85,55,240,195,80,30,35,65,140,150,145,65,65,120,70,85,65,65,100,50,65,25\right]'
+const sampleWeights = '[115,145,135,130,115,70,30,35,250,140,65,235,125,180,135,55,75,75,75,90,295,285,210,170,150,75,115,95,75,95,235,115,40,25,60,30,190,135,120,120,150,25,80,90,35,85,55,240,195,80,30,35,65,140,155,145,65,65,120,70,85,65,65,100,50,65,25]';
+const sampleWeightsOne = '[115,145,135,130,115,70,30,35,250,140,65,235,125,180,135,55,75,75,75,90,295,285,210,170,150,75,115,95,75,95,235,110,40,25,60,30,190,135,120,120,150,25,85,90,35,85,55,240,195,80,30,35,65,140,150,145,65,65,120,70,85,65,65,100,50,65,25]';
 
 describe('findExcercises', () => {
-    
+    describe('array', () => {
+        const results = findExercises(sampleExcercise, sampleWeights);
+
+        test('is an array', () => {
+            expect(Array.isArray(results)).toEqual(true);
+        });
+
+        test('is an array within length equal to input', () => {
+            expect(results).toHaveLength(sampleExcercise.length);
+        });
+
+        test('has objects as elements', () => {
+            for(let x of results) {
+                expect(typeof x).toEqual('object');
+            }
+        });
+
+        test('object has name, number, weight, and repRange property', () => {
+            for(let x of results) {
+                for(let y of ['name', 'number', 'weight', 'repRange']) {
+                    expect(x).toHaveProperty(y);
+                }
+            }
+        });
+
+        describe('number', () => {
+            test('is a number', () => {
+                for(let x of results) {
+                    expect(typeof x.number).toEqual('number');
+                }
+            });
+
+            test('is relative to an excercise', () => {
+                for(let x of results) {
+                    const name = exercises[x.number - 3].name;
+
+                    expect(x.name).toBe(name);
+                }
+            });
+        });
+
+        describe('weight', () => {
+            test('is a weight', () => {
+                for(let x of results) {
+                    expect(typeof x.weight).toEqual('number');
+                }
+            });
+
+            test('is relative to an excercise weight', () => {
+                for(let x of results) {
+                    const weight = exercises[x.number - 3].weight;
+
+                    expect(x.weight).toBe(weight);
+                }
+            });
+        });
+
+        describe('name', () => {
+            test('is a string', () => {
+                for(let x of results) {
+                    expect(typeof x.name).toEqual('string');
+                }
+            });
+
+            test('is relative to an excercise', () => {
+                for(let x of results) {
+                    const muscleGroup = exercises[x.number - 3].muscleGroup;
+
+                    expect(muscleGroup.some(muscleOne => x.muscles.some(muscleTwo => muscleOne == muscleTwo))).toBe(true);
+                }
+            });
+        });
+
+        test('excercise do not repeat', () => {
+            for(let x = 0; x < 1000; x++) {
+                const resultTemp = findExercises(sampleExcercise, sampleWeights);
+
+                for(let y = 0; y < resultTemp.length; y++) {
+                    const name = resultTemp[y].name;
+
+                    resultTemp[y].name = '';
+
+                    expect(resultTemp.findIndex(excer => excer.name == name)).toBe(-1);
+                }
+            }
+        });
+    });
+
+    describe('Errors', () => {
+        describe('first parameter', () => {
+            test('throws with non-array data types', () => {
+                for(let x of [true, 'string', 69, null, undefined, {name: 'john', age: 69}]) {
+                    expect(() => {
+                        findExercises(x, sampleWeights);
+                    }).toThrow(`${x} is not an array`);
+                }
+            });
+
+            test('throws with bad objects as elements', () => {
+                for(let x of [
+                    [6, 6, 4],
+                    ['s', 'string', 'str'],
+                    [{}, {name: 'jim', age: 26}, true, 'pizza']
+                ]) {
+                    expect(() => {
+                        findExercises(x, sampleWeights);
+                    }).toThrow('Bad objects as elements');
+                }
+            });
+        });
+
+        describe('second parameter', () => {
+            test('throws with non-string data types', () => {
+                for(let x of [true, ['string', false, 68], 69, null, undefined, {name: 'john', age: 69}]) {
+                    expect(() => {
+                        findExercises(sampleExcercise, x);
+                    }).toThrow(`${x} is not a string`);
+                }
+            });
+
+            test('throws with non-parsable string or non-array string', () => {
+                for(let x of ['string', 'cat', 'chicken', 'fish', "{'name': 'john', 'age': 69}"]) {
+                    expect(() => {
+                        findExercises(sampleExcercise, x);
+                    }).toThrow(`${x} is a bad string`);
+                }
+            });
+
+            test('throws with array string were thing are not numbers', () => {
+                for(let x of ["['string', false, 68]", "[true, 'string', 69, null, undefined]", "['s', 'string', 'str']"]) {
+                    expect(() => {
+                        findExercises(sampleExcercise, x);
+                    }).toThrow(`${x} is a bad string`);
+                }
+            });
+        });
+    });
 });
