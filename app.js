a248d199ea6ed5d607dc6b4cc913109752c4a50b
George Williams <george@localhost.localdomain>
2024-06-20 19:56:41 -0400

    intergrates find Excercises function

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const planDay = require("./modules/day-plan");
+const findExercises = require("./modules/excercise");
 const createSplit = require("./modules/split");
 const repsAndWeight = require("./modules/weight-rep");
 
 function generateWeek(maxLift, weights, days) {
     const weekDays = createSplit(days);
 
-    addWeights(weights);
-
     for(let x in weekDays) {
         weekDays[x] = planDay(weekDays[x]);
+        weekDays[x] = findExercises(weekDays[x], weights);
         
         if(weekDays[x].length > 0) {
             weekDays[x] = repsAndWeight(weekDays[x], maxLift)
